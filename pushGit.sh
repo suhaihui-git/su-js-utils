1c88e1d22232281900c6c3e2f9e70e214cbef3ea
suhaihui <1114527519@qq.com>
2024-12-30 14:19:36 +0800

    chore: pre-publish commit [skip ci]

---
+++ pushGit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 获取当前版本号
+version=$(node -p "require('./package.json').version")
+
+# 获取提交信息（如果没有提供，则使用版本号）
+commit_message=${1:-"v$version"}
+
+# 添加所有更改
+git add .
+
+# 提交更改
+git commit -m "$commit_message"
+
+# 添加版本标签
+git tag -a "v$version" -m "Version $version"
+
+# 推送到远程仓库
+git push origin main --force
+git push origin --tags
+
+echo "Successfully pushed version v$version to git with message: $commit_message" 
--- src/index.js
+++ src/index.js
@@ -11,3 +11,13 @@ export default {
   dom: domUtils,
   verify: verifyUtils,
 };
+
+// 单独导出
+export const { string, array, date, dom, verify } = utils;
+
+// 导出全部
+export * from "./stringUtils/index.js";
+export * from "./arrayUtils/index.js";
+export * from "./dateUtils/index.js";
+export * from "./domUtils/index.js";
+export * from "./verifyUtils/index.js";
