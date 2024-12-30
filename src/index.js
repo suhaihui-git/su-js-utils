3f63a8676f1d789f8be0d76a1a2b877831955c55
suhaihui <1114527519@qq.com>
2024-12-30 11:11:04 +0800

    chore: pre-publish commit [skip ci]

--- src/index.js
+++ src/index.js
@@ -3,17 +3,6 @@ import * as arrayUtils from "./arrayUtils/index.js";
 import * as dateUtils from "./dateUtils/index.js";
 import * as domUtils from "./domUtils/index.js";
 import * as verifyUtils from "./verifyUtils/index.js";
-
-// 导出所有工具类
-export { stringUtils, arrayUtils, dateUtils, domUtils, verifyUtils };
-
-// 导出具体函数，方便直接使用
-export * from "./stringUtils/index.js";
-export * from "./arrayUtils/index.js";
-export * from "./dateUtils/index.js";
-export * from "./domUtils/index.js";
-export * from "./verifyUtils/index.js";
-
 // 默认导出
 export default {
   string: stringUtils,
